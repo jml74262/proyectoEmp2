d7483adae9d04dec6e77bbae8c48196f79085e41
dan120929 <dan.salgado58d@gmail.com>
2022-05-12 00:55:01 -0500

    Agregue los archivos basicos del ts panas

---
+++ front/src/App.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+import * as ReactBootStrap from "react-bootstrap"
+
+
+function App() {
+  return (
+    <><ReactBootStrap.Container>
+      <ReactBootStrap.Navbar className="navbar navbar-dark bg-dark edit" >
+        
+      </ReactBootStrap.Navbar>
+    </ReactBootStrap.Container><div className="App">
+        <section className="Mzapato">
+        
+        </section>
+        <section className="Czapato">
+          
+        </section>
+
+      </div></>
+  );
+}
+
+export default App;
