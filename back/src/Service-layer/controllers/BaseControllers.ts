c7e785341bce495d668ee0a917a55afb86bd5901
Laura <lpr74382@udelasalle.edu.mx>
2022-05-12 10:40:55 -0500

    Se creo la carpeta de controllers y su base

---
+++ back/src/Service-layer/controllers/BaseControllers.ts
@@ -0,0 +1,31 @@
+import { Application, Router, Response } from 'express';
+
+export default abstract class BaseController {
+  protected readonly path: string;
+
+  protected readonly router: Router;
+
+  public constructor(path: string) {
+    this.path = path;
+    this.router = Router();
+
+    this.configureRouter();
+  }
+
+  protected abstract configureRouter(): void;
+
+  public mount(app: Application): void {
+    app.use(this.path, this.router);
+  }
+
+  public respond(res: Response, statusCode: number, data?: unknown): void {
+    // eslint-disable-next-line prettier/prettier
+    res.writeHead(statusCode, { "Content-type": "application/json"});
+
+    if (data) {
+      res.end(JSON.stringify(data));
+    } else {
+      res.end();
+    }
+  }
+}
